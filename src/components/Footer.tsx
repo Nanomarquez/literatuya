956375bbf8df247e59e3cb0619401c19ffd9a9bf
FedericoMarquez-IPG <Federico.Marquez@interpublic.com>
2025-06-08 13:20:14 -0300

    responsivber

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function Footer() {
   return (
     <footer className="border-t">
-      <div className="flex-1 container mx-auto p-4 flex items-center justify-between">
+      <div className="flex-1 container mx-auto p-4 flex items-center justify-between flex-col md:flex-row">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
             &copy; {new Date().getFullYear()} Literatuya. Todos los derechos
